6a76ed943d988fb1dee0cab342298793b29bedbd
V Echeveste <vaeb80@gmail.com>
2018-08-18 03:13:20 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -11,3 +11,7 @@ function accessElementInArray(array, index){
 function destructivelyRemoveElementFromBeginningOfArray(array){
   return array.shift()
 }
+function removeElementFromBeginningOfArray(array){
+  return array.slice(1)
+}
+function 
