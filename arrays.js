491d7e183edb790ad89c295026f698ae54c2220e
V Echeveste <vaeb80@gmail.com>
2018-08-18 02:54:47 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,6 +1,6 @@
 var chocolateBars =['snickers', 'hundred grand', 'kitkat', 'skittles'];
 function addElementToBeginningOfArray(array, item){
-  return
+  return [item, ...array]
 }
 function destructivelyAddElementToBeginningOfArray(array, item){
   return
