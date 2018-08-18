30006560a0643fb2cdd1b9a000b4076a988e7d07
V Echeveste <vaeb80@gmail.com>
2018-08-18 03:15:58 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -17,4 +17,6 @@ function removeElementFromBeginningOfArray(array){
 function destructivelyRemoveElementFromEndOfArray(array){
   return array.pop()
 }
-function 
+function removeElementFromEndOfArray(array){
+  return array.slice(0, array.length - 1)
+}
